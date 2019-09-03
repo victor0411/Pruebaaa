dd855f5b371fddf8e4b7db9ee7647534dc6c47b9
victor0411 <54162701+victor0411@users.noreply.github.com>
2019-09-02 21:06:32 -0500

    Agrgando datos area, perimetro, volumen,tipo
    clase Cubo

--- src/main/java/com/mycompany/figura/Cubo.java
+++ src/main/java/com/mycompany/figura/Cubo.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package com.mycompany.figura;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -91,6 +91,13 @@ public class Cubo extends Tridimencional{
         System.out.println("Area es igual a: " + (double)Math.round(area * 100)/100 );
         System.out.println("Perimetro es igual a: " + (double)Math.round(perimetro * 100)/100 );
         System.out.println("Volumen es igual a: " + (double)Math.round(volumen * 100)/100 );
+        contador = (Main.figura.size());
+        Main.figura.add(new ArrayList<Double>());
+        Main.figura.get(contador).add(4.0);
+        Main.figura.get(contador).add(area); 
+        Main.figura.get(contador).add(perimetro);
+        Main.figura.get(contador).add(volumen); 
+        Main.figura.get(contador).add((tipo*1.0));
     }
     
     
